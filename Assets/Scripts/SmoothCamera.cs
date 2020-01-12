a7abeb1b136e94d096f22ac6dbb72fef1cf62613
Sebastiaan <wiggler@live.nl>
2020-01-12 19:14:39 +0100

    Added the project and basic camera and driving

---
+++ Assets/Scripts/SmoothCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class SmoothCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float smoothTime;
+    [SerializeField]
+    private Vector3 offset;
+
+    private Vector3 velocity = Vector3.zero;
+
+    [SerializeField]
+    private float rotationSpeed;
+
+
+    void Update()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime * Time.deltaTime);
+        transform.position = smoothedPosition;
+        transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/VelocityManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class VelocityManager : MonoBehaviour
+{
+    [SerializeField]
+    private float maxVelo;
+
+    [SerializeField]
+    private float veloIncreases;
+
+    [SerializeField]
+    private float veloDecreases;
+
+    [SerializeField]
+    private float veloGravity;
+
+    [SerializeField]
+    private float rotationStrength;
+
+    private float currentVelo = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentVelo > maxVelo)
+        {
+            currentVelo = maxVelo;
+        }
+        else if (currentVelo < -(maxVelo/2))
+        {
+            currentVelo = -(maxVelo / 2);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            currentVelo += veloIncreases;
+        }
+        else if(currentVelo>0)
+        {
+            currentVelo -= veloGravity;
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            currentVelo -= veloDecreases;
+        }
+        else if (currentVelo<0)
+        {
+            currentVelo += veloGravity;
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down*rotationStrength);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * rotationStrength);
+        }
+
+        transform.Translate(Vector3.forward * currentVelo);
+    }
+}
